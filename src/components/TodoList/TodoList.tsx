d9f68fb126cf16a53202e21190dd913e7a614e02
Arslaan Qadus <azy2k9@gmail.com>
2020-05-01 11:30:50 +0100

    Fixed mark todo as completed bug

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -10,13 +10,20 @@ export interface Props {
 const TodoList = ({ todos, setTodos }: Props) => {
     const toggleCompletedStatus = (todo: TodoType) => {
         setTodos((prevTodos: TodoType[]) => {
-            prevTodos.reduce((acc, current) => { 
+            return prevTodos.reduce((acc: TodoType[], current: TodoType) => { 
                 if (current.id === todo.id) {
-                    current.completed_status = !current.completed_status;
+                    const newTodo: TodoType = {
+                        id: current.id,
+                        todo: current.todo,
+                        completed_status: !current.completed_status
+                    }
+                    acc = [...acc, newTodo];
+                }
+                else {
+                    acc.push(current);
                 }
                 return acc;
             }, []);
-            return prevTodos;
         })
     }
 
